14178fcf88c0235106160982ee88e5c4ae7ce4a9
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-02-05 15:49:39 -0300

    att

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ const navMenu = document.getElementById('nav-menu'),
         navMenu.classList.remove('show')
     })
 
-    document.addEventListener('mousemove', move)
-
+    document.addEventListener('mousemove', move);
     function move(e){
-        this.querySelectorALL('.move').forEach(layer =>{
-            const speed = layer.getaAttribute('data-speed')
-
-            const x = (window.i)
+        this.querySelectorAll('.move').forEach(layer =>{
+            const speed = layer.getAttribute('data-speed')
+    
+            const x = (window.innerWidth - e.pageX*speed)/120
+            const y = (window.innerHeight - e.pageY*speed)/120
+    
+            layer.style.transform = `translateX(${x}px) translateY(${y}px)`
         })
     }
+    
+    gsap.from('.nav-logo, .nav-toggle', {opacity: 0, duration: 1, delay:2, y: 10})
+    gsap.from('.nav-item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+    
+    // HOME
+    gsap.from('.home-title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+    gsap.from('.home-description', {opacity: 0, duration: 1, delay:1.8, y: 30})
+    gsap.from('.home-button', {opacity: 0, duration: 1, delay:2.1, y: 30})
+    gsap.from('.home-img', {opacity: 0, duration: 1, delay:1.3, y: 30})
