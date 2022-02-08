a68ea0204eaebe6da3c1c7aa56db8ddca9b8b32d
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-02-08 13:19:42 -0300

    salvando

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
+
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
 
 // SHOW
-toggleMenu.addEventListener('click', ()=>{
+toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show')
 })
 
-
-closeMenu.addEventListener('click', ()=>{
+// HIDDEN
+closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
 
-
+/*===== MOUSEMOVE HOME IMG =====*/
 document.addEventListener('mousemove', move);
-function move(e){
-    this.querySelectorAll('.move').forEach(layer =>{
+function move(e) {
+    this.querySelectorAll('.move').forEach(layer => {
         const speed = layer.getAttribute('data-speed')
 
-        const x = (window.innerWidth - e.pageX*speed)/120
-        const y = (window.innerHeight - e.pageY*speed)/120
+        const x = (window.innerWidth - e.pageX * speed) / 120
+        const y = (window.innerHeight - e.pageY * speed) / 120
 
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
-}
+    })}
 
-/*===== GSAP ANIMATION =====*/
-// NAV
-gsap.from('.nav-logo, .nav-toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav-item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
-
-// HOME
-gsap.from('.home-title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home-description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home-button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home-img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+ 
+    function comprarAlert (){
+        swal("Entregas somente em Governador Valadares")}
+          
+            
