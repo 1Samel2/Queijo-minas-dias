94982305aebb732150c68cd0602308ced6a38bb9
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-02-11 15:13:18 -0300

    alguns teste na responsive do formulario

--- index.js
+++ index.js
@@ -27,11 +27,3 @@ function move(e){
     })
 }
 
-/* find the error  */
-gsap.from('.nav-logo, .nav-toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav-item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
-
-gsap.from('.home-title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home-description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home-button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home-img', {opacity: 0, duration: 1, delay:1.3, y: 30})
